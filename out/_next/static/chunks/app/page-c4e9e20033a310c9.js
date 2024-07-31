6c0ff744041a88fa61574b9d16032f8317600c8f
Tu Van <tu.van@stepmediasoftware.com>
2024-07-31 11:22:01 +0700

    cpanel deploy nextjs static file 2

---
+++ out/_next/static/chunks/app/page-c4e9e20033a310c9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{5302:function(e,n,s){Promise.resolve().then(s.bind(s,6530)),Promise.resolve().then(s.bind(s,2928))}},function(e){e.O(0,[291,928,971,23,744],function(){return e(e.s=5302)}),_N_E=e.O()}]);
